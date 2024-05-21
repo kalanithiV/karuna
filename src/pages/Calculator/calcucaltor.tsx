03752f7d40fbbc877aa1649c7f9bb3043e2f95e5
karunakaran <karunakaran.p@nextbraintech.com>
2024-05-21 19:33:53 +0530

    calculator page remainig issues fixed

--- src/pages/Calculator/calcucaltor.tsx
+++ src/pages/Calculator/calcucaltor.tsx
@@ -1,129 +1,208 @@
 import React, { useState } from "react";
 import "../Calculator/calculator.css";
 import UndoIcon from '@mui/icons-material/Undo';
-const Calculator:React.FC=()=>{
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+interface CalculatorDetails {
+  id: number,
+  operators: string,
+  type: string
+}
+
+const Calculator: React.FC = () => {
   const [result, setResult] = useState("0");
-
-  const buttonClick = (value: string) => {
-    if (result === "0") {
-      setResult(value);
-    } else {
-      setResult(result + value);
+  const listing: CalculatorDetails[] = [
+    {
+      id: 1,
+      operators: "(",
+      type: "condition"
+    },
+    {
+      id: 2,
+      operators: "(",
+      type: "condition"
+    },
+    {
+      id: 3,
+      operators: "u",
+      type: "oneClear"
+    },
+    {
+      id: 4,
+      operators: "AC",
+      type: "clear"
+    },
+    {
+      id: 5,
+      operators: "1",
+      type: "Number"
+    },
+    {
+      id: 6,
+      operators: "2",
+      type: "Number"
+    },
+    {
+      id: 7,
+      operators: "3",
+      type: "Number"
+    },
+    {
+      id: 8,
+      operators: "/",
+      type: "condition"
+    },
+    {
+      id: 9,
+      operators: "4",
+      type: "Number"
+    },
+    {
+      id: 10,
+      operators: "5",
+      type: "Number"
+    },
+    {
+      id: 11,
+      operators: "6",
+      type: "Number"
+    },
+    {
+      id: 12,
+      operators: "*",
+      type: "condition"
+    },
+    {
+      id: 13,
+      operators: "7",
+      type: "Number"
+    },
+    {
+      id: 14,
+      operators: "8",
+      type: "Number"
+    },
+    {
+      id: 15,
+      operators: "9",
+      type: "Number"
+    },
+    {
+      id: 16,
+      operators: "-",
+      type: "condition"
+    },
+    {
+      id: 17,
+      operators: "0",
+      type: "Number"
+    },
+    {
+      id: 18,
+      operators: ".",
+      type: "Number"
+    },
+    {
+      id: 19,
+      operators: "=",
+      type: "equal"
+    },
+    {
+      id: 20,
+      operators: "+",
+      type: "condition"
     }
-  };
+  ];
 
-  const clearResult = () => {
-    setResult("0");
+  const buttonClick = (value: string, type: string) => {
+    if (type === "Number" || type === "condition") {
+      if (result === "0") {
+        setResult(value);
+      } else {
+        setResult(result + value);
+      }
+    } else if (type === "clear") {
+      setResult("0");
+    } else if (type === "oneClear") {
+      setResult(result.slice(0, -1) || "0");
+    } else if (type === "equal") {
+      try {
+        const finalValue = eval(result);
+        const fourDigits = String(finalValue).slice(0, 4)
+        const remainingvalue = String(finalValue).slice(0, 3)
+
+        console.log("eeeee", remainingvalue)
+        const fullvalue = fourDigits + '.' + remainingvalue
+        setResult(fullvalue);
+        console.log("valueeee fixed", finalValue)
+        if (finalValue === Infinity) {
+          toast.error('Devision by zero is undefined', {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            // transition: Bounce,
+          });
+          setResult("0")
+        }
+      } catch (err) {
+        alert("incorrect");
+
+      }
+    }
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setResult(e.target.value);
-    
   };
 
-  const finalResult = () => {
-    try {
-      setResult(eval(result));
-    } catch (err) {
-      alert("Incorrect calculation, please enter correct values");
-    }
-  };
-  const EraseButton =()=>{
-    setResult(result.slice(0,-1))
-}
   return (
     <>
-      <table>
-        <tbody>
-          <tr>
-            <td colSpan={4}>
-              <input type="text" value={result} onChange={handleChange} />
-              
-            </td>
-            {/* <td >
-            <button onClick={EraseButton} className="expressions"><UndoIcon /></button>
-            </td>  */}
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => buttonClick('(')} className="expressions">(</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick(')')} className="expressions">)</button>
-            </td>
-            {/* <td>
-              <button onClick={() => buttonClick('%')} className="expressions">%</button>
-            </td> */}
-              <td >
-            <button onClick={EraseButton} className="expressions undoIcon"><UndoIcon /></button>
-            </td> 
-            <td>
-              <button onClick={clearResult} className="expressions">AC</button>
-            </td>
-            {/* <td >
-            <button onClick={EraseButton} className="expressions undoIcon"><UndoIcon /></button>
-            </td>  */}
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => buttonClick('1')}>1</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('2')}>2</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('3')}>3</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('/')} className="expressions">/</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => buttonClick('4')}>4</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('5')}>5</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('6')}>6</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('*')} className="expressions">*</button>
-            </td>
-          </tr>
-          <tr>
-        
-            <td>
-              <button onClick={() => buttonClick('7')}>7</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('8')}>8</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('9')}>9</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('-')} className="expressions">-</button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <button onClick={() => buttonClick('0')}>0</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('.')}>.</button>
-            </td>
-            <td>
-              <button onClick={finalResult} className="equalBut">=</button>
-            </td>
-            <td>
-              <button onClick={() => buttonClick('+')} className="expressions">+</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="calculator">
+
+
+        <div className="calculator_operators">
+          <input type="text" value={result} onChange={handleChange} />
+          {listing.map((items) => (
+            <button
+              key={items.id}
+              onClick={() => buttonClick(items.operators, items.type)}
+              className={
+                items.type === "equal"
+                  ? "equalBut"
+                  : items.type === "condition"
+                    ? "expressions"
+                    : items.type === "clear"
+                      ? "expressions"
+                      : items.type === "oneClear"
+                        ? "expressions undoIcon"
+                        : ""
+              }
+            >
+              {items.operators === "u" ? <UndoIcon /> : items.operators}
+            </button>
+
+          ))}
+        </div>
+      </div>
+      <ToastContainer
+        position="top-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      ></ToastContainer>
     </>
   );
-}
+};
+
 export default Calculator;
